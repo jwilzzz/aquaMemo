9d87ba323aebdf12f25b768a9b4739f7ad523e58
wilsonjh2 <40837449+wilsonjh2@users.noreply.github.com>
2018-07-04 14:01:58 -0400

    Add files via upload

---
+++ JS/script1.js
@@ -0,0 +1,11 @@
+
+setTimeout(wakeUpUser, 3000);
+function wakeUpUser(){
+alert("Are you going to stare at this boring page forever?");
+}
+document.write("Another scoop!");
+//console log is displayed in developer view
+console.log("ANOTHER SCOOP!");
+
+<script type="text/javascript">
+alert("Hello World!");
---
+++ JS/script2.js
@@ -0,0 +1,17 @@
+var guess;
+guess = prompt("Password:");
+do{
+
+
+if(guess == "qwerty")
+{
+alert("Welcome!");
+}
+
+else if(guess != "qwerty")
+{
+alert("Incorrect! " + guess + " does not match the password ");
+guess = prompt("Password:");
+}
+while(quess != "qwerty")}
+
